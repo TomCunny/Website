bffb412fb9aae12b0f9e6ff59050758396625b6e
TomCunny <mrt.m.cunningham93@gmail.com>
2021-10-05 21:43:14 +0100

    Add files via upload

--- website/app.py
+++ website/app.py
@@ -1,5 +1,9 @@
+from os import name
 from flask import Flask
 from app_Blueprint import app_Blueprint
 
 app = Flask(__name__,static_folder="./images")
 app.register_blueprint(app_Blueprint)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- website/app_Blueprint.py
+++ website/app_Blueprint.py
@@ -1,22 +1,20 @@
-from flask import Blueprint, render_template
+from os import name
+from flask import Blueprint, render_template, redirect, url_for
 
 
 
 
 app_Blueprint = Blueprint("app_Blueprint",__name__)
 
-#variable
-tshirt = [{'name':'top','price': '15', 'image': 'images/tshirt.jpeg'}]
 
 @app_Blueprint.route('/')
 def index():
-    return render_template("index.html",tshirt=tshirt)
+    return render_template("index.html")
 
 
 @app_Blueprint.route('/order')
 def about():
     return render_template("order.html")
-
 
 
 @app_Blueprint.route('/products')
