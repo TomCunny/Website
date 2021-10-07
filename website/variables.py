cf850c38287f8d168741a46396c03bbc16d70683
TomCunny <mrt.m.cunningham93@gmail.com>
2021-10-07 01:33:00 +0100

    Add files via upload

---
+++ website/variables.py
@@ -0,0 +1,9 @@
+
+vendor_name="DCUK"
+
+
+benzos = [
+    {"name" : "Clonazepam", "price" : "20", "desc":"2mg pills by Galenika", "catergory" : "Benzodiazepine"},
+    {"name" : "Diaz", "price" : "30","desc" : "10mg pills by Galenika", "catergory" : "Benzodiazepine"},
+    {"name" : "Xanax", "price" : "40","desc" :"1mg pills by Galenika", "catergory" : "Benzodiazepine"}
+]
